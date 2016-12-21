faa60400179aec087238099f6f7a352b71ec3838
Wojtek <wojtek@niom.pl>
2016-12-21 03:04:03 +0100

    Add business rules to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :validatable
 
   enum status: [:normal, :premium, :premium_plus]
+
+  def allowed?(slug)
+    return false if slug.nil?
+    if normal?
+      slug.size < 15
+    elsif premium?
+      slug.size < 15 || slug.include?('books')
+    else
+      premium_plus?
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,29 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   before { build(:user) }
+  let(:premium_plus_user) { create(:user, status: :premium_plus) }
+  let(:premium_user) { create(:user, status: :premium) }
+  let(:normal_user) { create(:user, status: :normal) }
 
   it { should validate_presence_of(:email) }
   it { should validate_presence_of(:password) }
   it { should define_enum_for(:status).with(normal: 0, premium: 1, premium_plus: 2) }
+
+  context 'premium_plus user' do
+    it { expect(premium_plus_user.allowed?('desk-calendars')).to be true }
+    it { expect(premium_plus_user.allowed?('books-hardcover')).to be true }
+    it { expect(premium_plus_user.allowed?('compliment-slips')).to be true }
+  end
+
+  context 'premium user' do
+    it { expect(premium_user.allowed?('desk-calendars')).to be true }
+    it { expect(premium_user.allowed?('books-hardcover')).to be true }
+    it { expect(premium_user.allowed?('compliment-slips')).to be false }
+  end
+
+  context 'normal user' do
+    it { expect(normal_user.allowed?('desk-calendars')).to be true }
+    it { expect(normal_user.allowed?('books-hardcover')).to be false }
+    it { expect(normal_user.allowed?('compliment-slips')).to be false }
+  end
 end
